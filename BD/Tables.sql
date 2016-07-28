bf648e5d6b2270fe6468cfee4249c483b203ab85
pauescarcia <paulina_151090@hotmail.com>
2016-07-28 17:40:40 -0500

    Alter table Actividad

--- BD/Tables.sql
+++ BD/Tables.sql
@@ -38,21 +38,58 @@ WITH (
 ALTER TABLE trixdiscover."Categoria"
   OWNER TO "trixDiscover";
 
+--CREATE TABLE trixdiscover."Actividad"
+--(
+--   "idActividad" bigserial, 
+--   "Nombre" text, 
+--   "Descripcion" text, 
+--   "Disponibilidad" text, 
+--   "Duracion" text, 
+--   "Costo" integer, 
+--   "idCategoria" bigint, 
+--   CONSTRAINT "Pk_Actividad" PRIMARY KEY ("idActividad"), 
+--   CONSTRAINT "Fk_Actividad" FOREIGN KEY ("idCategoria") REFERENCES trixdiscover."Categoria" ("idCategoria") ON UPDATE NO ACTION ON DELETE NO ACTION
+--) 
+--WITH (
+--  OIDS = FALSE
+--)
+--;
+--ALTER TABLE trixdiscover."Actividad"
+--  OWNER TO "trixDiscover";
+
+-- Table: trixdiscover."Actividad"
+
+-- DROP TABLE trixdiscover."Actividad";
+
 CREATE TABLE trixdiscover."Actividad"
 (
-   "idActividad" bigserial, 
-   "Nombre" text, 
-   "Descripcion" text, 
-   "Disponibilidad" text, 
-   "Duracion" text, 
-   "Costo" integer, 
-   "idCategoria" bigint, 
-   CONSTRAINT "Pk_Actividad" PRIMARY KEY ("idActividad"), 
-   CONSTRAINT "Fk_Actividad" FOREIGN KEY ("idCategoria") REFERENCES trixdiscover."Categoria" ("idCategoria") ON UPDATE NO ACTION ON DELETE NO ACTION
-) 
-WITH (
-  OIDS = FALSE
+  "idActividad" bigserial NOT NULL,
+  "Nombre" text,
+  "Descripcion" text,
+  "Disponibilidad" text,
+  "Duracion" text,
+  "Costo" integer,
+  "idCategoria" bigint,
+  "idPrestador" bigint,
+  CONSTRAINT "Pk_Actividad" PRIMARY KEY ("idActividad"),
+  CONSTRAINT "FK_ActividadesPrestador" FOREIGN KEY ("idPrestador")
+      REFERENCES trixdiscover."Prestador" ("IdPrestador") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "Fk_Actividad" FOREIGN KEY ("idCategoria")
+      REFERENCES trixdiscover."Categoria" ("idCategoria") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
-;
+WITH (
+  OIDS=FALSE
+);
 ALTER TABLE trixdiscover."Actividad"
   OWNER TO "trixDiscover";
+
+-- Index: trixdiscover."FKI_ActividadesPrestador"
+
+-- DROP INDEX trixdiscover."FKI_ActividadesPrestador";
+
+CREATE INDEX "FKI_ActividadesPrestador"
+  ON trixdiscover."Actividad"
+  USING btree
+  ("idPrestador");
